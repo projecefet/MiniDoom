5d621ccb50afac0171594f66b0b27679acea8d94
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-08-26 15:31:41 -0300

    Update player.hpp

--- TP-3/MiniDoom/src/headers/player.hpp
+++ TP-3/MiniDoom/src/headers/player.hpp
@@ -1,31 +1,38 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
-#include <iostream>
-using std::cout;
-using std::endl;
-
 #include "map.hpp"
+#include "bullet.hpp"
 
 class Player {
  private:
   sf::Vector2f pos;
   sf::RectangleShape rect;
+
   int jumpCooldown = 200;
   int jump_HoldTime = 0;
   const int max_Jump_HoldTime = 30;
   bool jumped = false;
+
+
 
   sf::Clock playerClock;
 
   float velocity = 200;
 
  public:
+  enum direction{
+  		left,
+  		right
+  	};
+  int direction = right;
+  int shootCooldown = 0;
+
   Player();
   ~Player();
 
 
   void update(Map *map);
+  void shoot(std::vector<Bullet*> &bullets);
   void render(sf::RenderWindow *i_window);
   void die();
 
