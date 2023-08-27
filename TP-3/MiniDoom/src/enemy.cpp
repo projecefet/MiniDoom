592e94191b11b4993441cab9cd517ac17a8d8e20
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-08-27 12:45:25 -0300

    Update enemy.cpp

--- TP-3/MiniDoom/src/enemy.cpp
+++ TP-3/MiniDoom/src/enemy.cpp
@@ -1,42 +1,60 @@
 #include "headers/enemy.hpp"
 
-
-Enemy::Enemy(){
-	this->position = Vector2f(1 * 256, 1 * 80);
+Enemy::Enemy() {
+	this->position = sf::Vector2f(1 * 256, 1 * 80);
 
 	this->shape.setSize(sf::Vector2f(16, 16));
 	this->shape.setFillColor(sf::Color::Blue);
 	this->shape.setPosition(this->position);
+	this->hitbox = shape.getGlobalBounds();
 }
 
-Enemy::~Enemy(){
+Enemy::~Enemy() {
 }
 
-void Enemy::update(Map *map, Player * jogador){
-	if ((position.x - jogador->position.x) <= 140 || distanceAllowed == true){
+void Enemy::update(Map *map, Player *jogador) {
+	if ((position.x - jogador->position.x) <= 140 || distanceAllowed == true) {
 		distanceAllowed = true;
 		//a diferença entre a posicao do jogador e do inimigo != 0
-		if((jogador->position.x - position.x) < -18){ //esquerda
+		if ((jogador->position.x - position.x) < -18) { //esquerda
 			shape.move(-followspeed, 0.f);
 			newPosition = shape.getPosition();
 			position = newPosition;
-		}
-		else if ((jogador->position.x - position.x) > 18){ //direita
+		} else if ((jogador->position.x - position.x) > 18) { //direita
 			shape.move(followspeed, 0.f);
 			newPosition = shape.getPosition();
 			position = newPosition;
 		}
 
 		distanceAllowed = true;
-		cout << "pos Player: " << jogador->position.x << endl;
-		cout << "pos Inimigo: " << newPosition.x << endl;
-		cout << "pos Inimigo: " << newPosition.y << endl;
+
+		hitbox = shape.getGlobalBounds();
 
 	}
 }
 
-void Enemy::die() {
-	this->position = sf::Vector2f(2 * 16, 3 * 16);
+bool Enemy::gotShot(Bullet *&bullet) {
+
+	int auxiliaryIncrement = 0;
+	if (bullet->hitbox.intersects(hitbox)) {
+		if (idBulletsHit.size() != 0) {
+			for (int i = 0; i < idBulletsHit.size(); i++) {
+				if (bullet->id != idBulletsHit.at(i)) {
+					auxiliaryIncrement++;
+				}
+			}
+			if (auxiliaryIncrement == idBulletsHit.size()) {
+				idBulletsHit.push_back(bullet->id);
+			}
+		} else {
+			idBulletsHit.push_back(bullet->id);
+		}
+	}
+	return bullet->hitbox.intersects(hitbox);
+}
+
+bool Enemy::checkDeath() {
+	return idBulletsHit.size() >= 10;
 }
 
 void Enemy::render(sf::RenderWindow *i_window) {
