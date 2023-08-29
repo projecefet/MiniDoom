13985526492ead43bad684e5574f2ba6f0cbca44
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-08-29 19:46:52 -0300

    Update map.cpp

--- TP-3/MiniDoom/src/map.cpp
+++ TP-3/MiniDoom/src/map.cpp
@@ -1,41 +1,113 @@
-#pragma once
+#include "headers/map.hpp"
+
+using namespace sf;
+
+// Constructors / Destructors
 
 #include <iostream>
-using std::cout;
-using std::endl;
-#include <vector>
-using std::vector;
 
-#include <string>
-using std::string;
+Map::Map(string mapName, string xmlPath) {
+  /*
+  - Loads map images
+  - Get every pixel and maps it on mapColorFunction
+  - Push the maped value to class map vector
+  */
 
-#include <SFML/Graphics.hpp>
-#include <cstring>
+  tinyxml2::XMLDocument doc;
+  doc.LoadFile(xmlPath.c_str());
 
-#include "../tinyxml2.h"
+  tinyxml2::XMLElement *rootNode = doc.RootElement();
+  tinyxml2::XMLElement *block = rootNode->FirstChildElement("block");
 
-struct Block {
-  string color;
-  sf::Vector2i position;
-  bool collision;
-  bool damage;
-};
+  // Colocando as informações do xml no array de colisão e na matriz de tiles
+  while (block) {
+    Block currentBlock;
 
-class Map {
- private:
-  vector<vector<Block>> mapTiles;
+    // Informações do block
+    currentBlock.color = block->FirstChildElement("color")->GetText();
+    tinyxml2::XMLElement *position = block->FirstChildElement("position");
 
- public:
-  vector<Block> mapColorFunction;
+    int x = std::atoi(position->FirstChildElement("x")->GetText());
+    int y = std::atoi(position->FirstChildElement("y")->GetText());
 
-  // Constructor / Destructor
-  Map(string mapName, string xmlPath);
-  ~Map();
+    currentBlock.position = sf::Vector2i(x, y);
 
-  // Functions
-  void render(sf::RenderWindow *i_window);
+    currentBlock.collision = strcmp(block->FirstChildElement("collision")->GetText(), "true");
+    currentBlock.damage = strcmp(block->FirstChildElement("damage")->GetText(), "true");
 
-  // Collision functions
-  bool doesCollide(sf::Vector2i pos);
-  bool dealsDamage(sf::Vector2i pos);
-};
+    this->mapColorFunction.push_back(currentBlock);
+
+    block = block->NextSiblingElement("block");
+
+    // Colisão
+  }
+
+  // Lendo como o mapa é
+  Image map;
+  map.loadFromFile(mapName);
+
+  Vector2u mapSize = map.getSize();
+
+  for (unsigned int y = 0; y < mapSize.y; y++) {
+    vector<Block> yAxis;
+    for (unsigned int x = 0; x < mapSize.x; x++) {
+      Color color = map.getPixel(x, y);
+
+      for (unsigned int i = 0; i < this->mapColorFunction.size(); i++) {
+        if (this->mapColorFunction[i].color == std::to_string(color.r) + "," +
+                                                   std::to_string(color.g) + "," +
+                                                   std::to_string(color.b)) {
+          yAxis.push_back(this->mapColorFunction[i]);
+        }
+      }
+    }
+
+    this->mapTiles.push_back(yAxis);
+  }
+}
+
+Map::~Map() {}
+
+// Functions
+
+void Map::render(RenderWindow *i_window) {
+  /*
+    @return void
+
+    Draws map vector on screen
+  */
+
+  const int OFFSET = 16;
+
+  Texture gameTexture;
+  gameTexture.loadFromFile("resources/terrain-tilemap.png");
+
+  RectangleShape square;
+  square.setSize(Vector2f(OFFSET, OFFSET));
+
+  square.setTexture(&gameTexture);
+
+  for (unsigned int y = 0; y < this->mapTiles.size(); y++) {
+    for (unsigned int x = 0; x < this->mapTiles[y].size(); x++) {
+      square.setTextureRect(IntRect(OFFSET * mapTiles[y][x].position.x,
+                                    OFFSET * mapTiles[y][x].position.y, OFFSET, OFFSET));
+      square.setPosition(Vector2f(OFFSET * x, OFFSET * y));
+
+      i_window->draw(square);
+    }
+  }
+}
+
+bool Map::doesCollide(sf::Vector2i pos) {
+  if (mapTiles[pos.y][pos.x].collision) {
+    return true;
+  }
+  return false;
+}
+
+bool Map::dealsDamage(sf::Vector2i pos) {
+  if (mapTiles[pos.y][pos.x].damage) {
+    return true;
+  }
+  return false;
+}
