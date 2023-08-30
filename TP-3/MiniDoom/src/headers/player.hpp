2c808546b269296d92de6e452efe2709a988388d
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-08-30 17:31:17 -0300

    Update player.hpp

--- TP-3/MiniDoom/src/headers/player.hpp
+++ TP-3/MiniDoom/src/headers/player.hpp
@@ -5,36 +5,42 @@
 #include "movable.hpp"
 
 class Player: public Movable {
- private:
+private:
 
-  int jumpCooldown = 200;
-  int jump_HoldTime = 0;
-  const int max_Jump_HoldTime = 30;
-  bool jumped = false;
+	int jumpCooldown = 200;
+	int jump_HoldTime = 0;
+	const int max_Jump_HoldTime = 30;
+	bool jumped = false;
 
+	int shootCooldown = 0;
+	int max_ShootCooldown = 80;
 
+	enum direction {
+		left, right
+	};
+	int direction = right;
 
-  sf::Clock playerClock;
+	sf::Clock playerClock;
 
-  float velocity = 200;
+	float velocity = 200;
 
- public:
-  enum direction{
-  		left,
-  		right
-  	};
-  int direction = right;
-  int shootCooldown = 0;
-  sf::Vector2f position;
+public:
 
-  Player();
-  ~Player();
+	sf::Vector2f position;
 
+	Player();
+	~Player();
 
-  void update(Map *map);
-  void shoot(std::vector<Bullet*> &bullets, int &id);
-  void render(sf::RenderWindow *i_window);
-  void die();
+	void move(Map *map);
+	void shoot(std::vector<Bullet*> &bullets, int &id);
+	void jump(Map *map);
+	void walkLeft(Map *map);
+	void walkRight(Map *map);
+	void groundColision(Map *map);
+	void died(Map *map);
+	void update(sf::RenderWindow *window, Map *map);
+	void render(sf::RenderWindow *i_window);
+	void die();
 
-  sf::Vector2f getPosition();
+	sf::Vector2f getPosition();
 };
