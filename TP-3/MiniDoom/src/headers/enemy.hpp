166743121398da18e80447c931d1ee3e1a7fbcb0
ViniciusCandido0 <134893667+ViniciusCandido0@users.noreply.github.com>
2023-09-01 23:00:04 -0300

    Update enemy.hpp

--- TP-3/MiniDoom/src/headers/enemy.hpp
+++ TP-3/MiniDoom/src/headers/enemy.hpp
@@ -1,6 +1,9 @@
 #ifndef SRC_HEADERS_ENEMY_HPP_
 #define SRC_HEADERS_ENEMY_HPP_
 
+//template<typename S>;
+
+#include <vector>
 #include "map.hpp"
 #include "movable.hpp"
 #include "player.hpp"
@@ -12,23 +15,35 @@
 using std::cout;
 using std::endl;
 
+
 class Enemy: public Movable {
 private:
 	std::vector<int> idBulletsHit;
 public:
-    bool distanceAllowed = false;
-    float followspeed = 0.1f;
-    int enemyId;
+    Enemy(){};
+    ~Enemy(){};
 
-    Enemy(int enemyId);
-    ~Enemy();
-
-	void update(Map *map, Player * jogador);
 	bool gotShot(Bullet *&bullet);
     void render(sf::RenderWindow *i_window);
     bool checkDeath();
 
     sf::Vector2f newPosition;
+};
+
+class Snake: public Enemy{ //inimigo estatico
+public:
+	Snake (int posx, int posy);
+	~Snake();
+};
+
+class Saint: public Enemy{ //inimigo do jogo que irá seguir o player
+public:
+	bool distanceAllowed = false;
+	float followspeed = 0.1f;
+
+	void update(Map *map, Player * jogador);
+	Saint();
+	~Saint();
 };
 
 #endif
