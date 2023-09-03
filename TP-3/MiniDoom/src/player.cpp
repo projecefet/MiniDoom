be40db849b9e404dd05b86d2d1ac3ca3837e62e4
Otto-abreu <130250345+Otto-abreu@users.noreply.github.com>
2023-09-03 14:12:35 -0300

    Update player.cpp

--- TP-3/MiniDoom/src/player.cpp
+++ TP-3/MiniDoom/src/player.cpp
@@ -7,24 +7,57 @@ Player::Player() {
 	this->shape.setSize(sf::Vector2f(16, 16));
 	this->shape.setFillColor(sf::Color::Transparent);
 	this->shape.setPosition(this->position);
+	this->hitbox = shape.getGlobalBounds();
 
-	walkingTextures[0].loadFromFile("characters/jao/walking_gun/0.png");
-	walkingTextures[1].loadFromFile("characters/jao/walking_gun/1.png");
-	walkingTextures[2].loadFromFile("characters/jao/walking_gun/2.png");
-	walkingTextures[3].loadFromFile("characters/jao/walking_gun/3.png");
+	facingRightTextures[0].loadFromFile("characters/jao/walking_gun/0.png");
+	facingRightTextures[1].loadFromFile("characters/jao/walking_gun/1.png");
+	facingRightTextures[2].loadFromFile("characters/jao/walking_gun/2.png");
+	facingRightTextures[3].loadFromFile("characters/jao/walking_gun/3.png");
 
-	sprite.setTexture(walkingTextures[textureIndex]);
+	facingLeftTextures[0].loadFromFile("characters/jao/walking_gun/a.png");
+	facingLeftTextures[1].loadFromFile("characters/jao/walking_gun/b.png");
+	facingLeftTextures[2].loadFromFile("characters/jao/walking_gun/c.png");
+	facingLeftTextures[3].loadFromFile("characters/jao/walking_gun/d.png");
+
+	sprite.setTexture(facingRightTextures[textureIndex]);
+
+	heartTexture.loadFromFile("characters/jao/life.png");
+
+	sf::Sprite heart1;
+	sf::Sprite heart2;
+	sf::Sprite heart3;
+
+	heart1.setTexture(heartTexture);
+	heart2.setTexture(heartTexture);
+	heart3.setTexture(heartTexture);
+
+	heartSprites.push_back(heart1);
+	heartSprites.push_back(heart2);
+	heartSprites.push_back(heart3);
 
 }
 
 Player::~Player() {
 
 }
+
+void Player::receiveDamage(sf::FloatRect rect) {
+	damageReceivedCooldown++;
+	if (hitbox.intersects(rect) && damageReceivedCooldown >= 2000) {
+		lifes--;
+		damageReceivedCooldown = 0;
+	}
+}
 void Player::updateAnimation() {
 
-	sprite.setTexture(walkingTextures[textureIndex]);
-//	cout << textureIndex << " - textureIndex" << endl;
+	if (direction == right) {
+		sprite.setTexture(facingRightTextures[textureIndex]);
+	} else if (direction == left) {
+		sprite.setTexture(facingLeftTextures[textureIndex]);
+	}
+
 	currentFrame++;
+
 	if (currentFrame == 15) {
 		if (textureIndex < 3) {
 			textureIndex++;
@@ -42,7 +75,6 @@ void Player::groundColision(Map *map) {
 	}
 }
 void Player::jump(Map *map) {
-
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)
 			&& (jumpCooldown >= 50 || jumped == true)) {
 		if (jumpCooldown >= 50) {
@@ -60,10 +92,12 @@ void Player::jump(Map *map) {
 			}
 			jump_HoldTime++;
 		}
+
 	} else {
 		jump_HoldTime = 0;
 		jumpCooldown++;
 		jumped = false;
+
 	}
 }
 
@@ -75,6 +109,7 @@ void Player::walkLeft(Map *map) {
 		this->position.x -= velocity
 				* this->playerClock.getElapsedTime().asSeconds();
 		direction = left;
+
 	}
 }
 
@@ -86,13 +121,14 @@ void Player::walkRight(Map *map) {
 		this->position.x += velocity
 				* this->playerClock.getElapsedTime().asSeconds();
 		direction = right;
+
 	}
 }
 
-void Player::died(Map *map) {
+void Player::hitTrap(Map *map) {
 	if (!map->dealsDamage(
 			sf::Vector2i(this->position.x / 16, this->position.y / 16))) {
-		this->die();
+		this->reSpawn();
 	}
 }
 void Player::move(Map *map) {
@@ -112,7 +148,7 @@ void Player::move(Map *map) {
 
 }
 
-void Player::shoot(std::vector<Bullet*> &bullets, int &id) {
+void Player::shoot(std::vector<Bullet*> &bullets, int &id, sf::Sound &sound) {
 	if (shootCooldown < max_ShootCooldown) {
 		shootCooldown++;
 	}
@@ -121,6 +157,7 @@ void Player::shoot(std::vector<Bullet*> &bullets, int &id) {
 		Bullet *newBullet = new Bullet(position, direction, id);
 		bullets.push_back(newBullet);
 		shootCooldown = 0;
+		sound.play();
 	}
 }
 
@@ -138,15 +175,16 @@ void Player::render(sf::RenderWindow *window) {
 					sf::Vector2f(400, 300)));
 } // 400/300
 
-void Player::die() {
+void Player::reSpawn() {
 	this->position = sf::Vector2f(2 * 16, 3 * 16);
 }
 
 void Player::update(sf::RenderWindow *window, Map *map) {
 	move(map);
-	died(map);
+	hitTrap(map);
 	updateAnimation();
 	render(window);
+	hitbox = shape.getGlobalBounds();
 }
 
 sf::Vector2f Player::getPosition() {
